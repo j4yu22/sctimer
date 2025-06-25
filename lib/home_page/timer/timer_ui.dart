d4a605dfcd8e584e154fdddd37e998bbd3ee8b5b
jayunderwood <jay.e.underwood@gmail.com>
2025-06-25 11:17:13 -0600

    fixed the icons being chinese. also added the 4 time modifier buttons, but I need to fix their placement

--- lib/home_page/timer/timer_ui.dart
+++ lib/home_page/timer/timer_ui.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'timer_logic.dart';
 import 'fullscreen_timer.dart';
 
+// remove time, dnf, +2 seconds, comment
 class TimerUI extends StatefulWidget {
   final bool isFullscreen;
   final void Function(bool)? onFullscreenChange;
@@ -68,27 +69,71 @@ class _TimerUIState extends State<TimerUI> {
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: GestureDetector(
-        onTapDown: _onTapDown,
-        onTapUp: _onTapUp,
-        onPanEnd: _onPanEnd,
+    return GestureDetector(
+      onTapDown: _onTapDown,
+      onTapUp: _onTapUp,
+      onPanEnd: _onPanEnd,
+      child: AspectRatio(
+        aspectRatio: 1, // force a square
         child: AnimatedContainer(
           duration: const Duration(milliseconds: 300),
           color: _primed ? Colors.yellow : Colors.grey[400],
-          alignment: Alignment.center,
-          child: ValueListenableBuilder<int>(
-            valueListenable: _logic.timeNotifier,
-            builder: (context, milliseconds, _) {
-              final seconds = (milliseconds / 1000).floor();
-              final ms = (milliseconds % 1000) ~/ 10;
-              final timeString = '$seconds.${ms.toString().padLeft(2, '0')}';
+          child: Stack(
+            children: [
+              // Centered timer text
+              Center(
+                child: ValueListenableBuilder<int>(
+                  valueListenable: _logic.timeNotifier,
+                  builder: (context, milliseconds, _) {
+                    final seconds = (milliseconds / 1000).floor();
+                    final ms = (milliseconds % 1000) ~/ 10;
+                    final timeString =
+                        '$seconds.${ms.toString().padLeft(2, '0')}';
 
-              return Text(
-                timeString,
-                style: const TextStyle(fontSize: 32, color: Colors.black87),
-              );
-            },
+                    return Text(
+                      timeString,
+                      style: const TextStyle(
+                        fontSize: 32,
+                        color: Colors.black87,
+                      ),
+                    );
+                  },
+                ),
+              ),
+
+              // Icon row at bottom center
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: Padding(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      IconButton(
+                        onPressed: () {},
+                        icon: const Icon(Icons.close),
+                        tooltip: 'Cancel time',
+                      ),
+                      IconButton(
+                        onPressed: () {},
+                        icon: const Icon(Icons.block),
+                        tooltip: 'Mark as DNF',
+                      ),
+                      IconButton(
+                        onPressed: () {},
+                        icon: const Icon(Icons.flag),
+                        tooltip: '+2 Second Penalty',
+                      ),
+                      IconButton(
+                        onPressed: () {},
+                        icon: const Icon(Icons.comment),
+                        tooltip: 'Add comment',
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       ),
